3d33acd3695f17e93571dc4de334d7366b179da2
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-02-12 08:07:42 -0300

    Splash Screen

---
+++ app/src/main/java/com/example/speedometer/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.speedometer
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#006600")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
